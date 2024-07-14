a807f0d1b69acabbece3c7815b568cbb51d21fdb
reymom <ramon_gs95@hotmail.com>
2024-07-14 08:39:14 +0200

    implement swap frontend and calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+"use client"
+
+import '@rainbow-me/rainbowkit/styles.css';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { ethers } from "ethers";
+import Image from 'next/image';
+import logo from '@/app/logo.svg'
+
+const usdcABI = [
+    {
+        constant: false,
+        inputs: [
+            { name: "_to", type: "address" },
+            { name: "_value", type: "uint256" },
+        ],
+        name: "transfer",
+        outputs: [{ name: "", type: "bool" }],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        constant: true,
+        inputs: [
+            { name: "_owner", type: "address" }
+        ],
+        name: "balanceOf",
+        outputs: [
+            { name: "balance", type: "uint256" }
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+    },
+];
+
+const USDC_CONTRACT_ADDRESS_CELO = "0x2F25deB3848C207fc8E0c34035B3Ba7fC157602B";
+const USDC_CONTRACT_ADDRESS_ARBITRUM = "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d";
+const USDC_CONTRACT_ADDRESS_ROOTSTOCK = "0x81532a7ab169efb8c3a0b6a173c8c4613a336eea";
+const RELAYER_ADDRESS = "0x632b39E5Fe4EAAFDF21601b2Bc206ca0f602C85A";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const { address, isConnected, chainId } = useAccount();
+    const [amount, setAmount] = useState("");
+    const [balance, setBalance] = useState("");
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    useEffect(() => {
+        const fetchBalance = async () => {
+            if (!window.ethereum || !isConnected || !address) return;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            await provider.send("eth_requestAccounts", []);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            let contractAddress = "";
+            if (chainId === 421614) {
+                contractAddress = USDC_CONTRACT_ADDRESS_ARBITRUM;
+            } else if (chainId === 44787) {
+                contractAddress = USDC_CONTRACT_ADDRESS_CELO;
+            } else if (chainId === 31) {
+                contractAddress = USDC_CONTRACT_ADDRESS_ROOTSTOCK;
+            } else {
+                console.error("Chain not supported");
+                return;
+            }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            const usdcContract = new ethers.Contract(contractAddress, usdcABI, provider);
+            const balance = await usdcContract.balanceOf(address);
+            setBalance(parseFloat(ethers.formatUnits(balance, 6)).toFixed(6).replace(/\.?0+$/, ""));
+        };
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+        fetchBalance();
+    }, [address, isConnected, chainId]);
+
+    const handleSwap = async () => {
+        if (!window.ethereum || !isConnected) {
+            throw new Error("No crypto wallet found. Please install it.");
+        }
+
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+
+        let contractAddress = "";
+        if (chainId == 421614) {
+            contractAddress = USDC_CONTRACT_ADDRESS_ARBITRUM
+        } else if (chainId == 44787) {
+            contractAddress = USDC_CONTRACT_ADDRESS_CELO
+        } else {
+            console.error("chain not supported");
+            return;
+        }
+
+        const usdcContract = new ethers.Contract(
+            contractAddress,
+            usdcABI,
+            signer
+        );
+        const transactionResponse = await usdcContract.transfer(
+            RELAYER_ADDRESS,
+            ethers.parseUnits(amount, 6)
+        );
+        console.log('Transaction response:', transactionResponse);
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="mt-32">
+                <div className="flex flex-col items-center justify-center space-y-4 my-10">
+                    <Image src={logo} alt="Logo" width={300} height={300} />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <ConnectButton showBalance={false} />
+                </div>
+                <div className="flex flex-col items-center justify-center space-y-4 my-10">
+                    <input
+                        type="text"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        placeholder="Amount"
+                        className="border border-gray-300 rounded-md p-2 text-black"
+                    />
+                    <button
+                        onClick={handleSwap}
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md"
+                    >
+                        Swap
+                    </button>
+                    <p className="text-2xl text-pink-600 font-bold mb-4">
+                        Balance: <span className="text-white text-shadow">{balance} USDC</span>
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
 }
